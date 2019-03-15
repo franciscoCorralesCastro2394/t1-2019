2a43691692e98160b0c1017a16b0377d16c8ccec
Francisco Corrales Castro <francisco.corrales.castro@gmail.com>
2019-03-15 15:49:48 -0600

    Cambios en corrasel

---
+++ ce-web/src/app/clases/noticia.ts
@@ -0,0 +1,10 @@
+export class Noticia {
+
+Id:Number;
+Titulo:String;
+Imagen:String;
+Descripcion:String;
+fechaCreacion:Date;
+ultimaModificacion:Date; 
+
+}
---
+++ ce-web/src/app/components/noticias-list/noticias-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-noticias-list',
+  templateUrl: './noticias-list.component.html',
+  styleUrls: ['./noticias-list.component.css']
+})
+export class NoticiasListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
