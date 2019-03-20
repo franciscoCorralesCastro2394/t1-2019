e711c8127939d876570c80d8321c3b01b7acc93e
Francisco Corrales Castro <francisco.corrales.castro@gmail.com>
2019-03-20 16:25:51 -0600

    cambios en carrucel

--- ce-web/src/app/components/noticias-list/noticias-list.component.ts
+++ ce-web/src/app/components/noticias-list/noticias-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataStorageService } from '../../services/data-storage.service';
 
 @Component({
   selector: 'app-noticias-list',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./noticias-list.component.css']
 })
 export class NoticiasListComponent implements OnInit {
+  private noticias:any[];  
+  constructor(private dataStorage:DataStorageService ) {
 
-  constructor() { }
+    this.getNoticias();
+   
+  }
+   
+ 
 
   ngOnInit() {
+  
   }
+
+   
+
+ 
+
+
+  getNoticias = () => {
+    this.noticias = this.dataStorage.getObjectValue('123');
+    console.log(this.noticias);
+    console.log("pruebas");
+
+  }
+
 
 }
--- ce-web/src/app/services/data-storage.service.ts
+++ ce-web/src/app/services/data-storage.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Noticia} from '../clases/noticia';
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -11,18 +12,58 @@ export class DataStorageService {
 
 setObjectValue = (key:string, objectValue:any)=>{
 
+  let noticias:Noticia[] = [];  
+
   let noti:Noticia = new Noticia;
   noti.Id = 1;
-  noti.Descripcion = 'proyecto cultural sostenible';
+  noti.Descripcion = 'proyecto cultural sostenible Nauyaka';
   noti.Titulo = 'Nauyaka';
-  noti.Imagen = 'src/assets/img/Cascada_azul_costa_rica_muralesyvinilos_26641659__Monthly_XL.jpg';
+  noti.Imagen = 'assets/img/Cascada_azul_costa_rica_muralesyvinilos_26641659__Monthly_XL.jpg';
   noti.fechaCreacion = new Date(Date.now());
   noti.ultimaModificacion = new Date(Date.now());
+
+  noticias.push(noti);
+
+  noti = new Noticia;
+  noti.Id = 2;
+  noti.Descripcion = 'proyecto cultural sostenible Talamanca';
+  noti.Titulo = 'Talamanca';
+  noti.Imagen = 'assets/img/1367498182_037809_1367498356_noticia_normal.jpg';
+  noti.fechaCreacion = new Date(Date.now());
+  noti.ultimaModificacion = new Date(Date.now())
+
+  noticias.push(noti);
+
+  
+  noti = new Noticia;
+  noti.Id = 3;
+  noti.Descripcion = 'proyecto cultural sostenible Bri-bri';
+  noti.Titulo = 'Bri-bri';
+  noti.Imagen = 'assets/img/maxresdefault.jpg';
+  noti.fechaCreacion = new Date(Date.now());
+  noti.ultimaModificacion = new Date(Date.now())
+
+  noticias.push(noti);
+
+  noti = new Noticia;
+  noti.Id = 4;
+  noti.Descripcion = 'proyecto cultural sostenible Cabecar';
+  noti.Titulo = 'Cabecar';
+  noti.Imagen = 'assets/img/nauyaca-waterfalls-horseback.jpg';
+  noti.fechaCreacion = new Date(Date.now());
+  noti.ultimaModificacion = new Date(Date.now())
+
+  noticias.push(noti);
+
 
   console.log(key + objectValue);
   console.log(noti);
   if (window.localStorage) {
-    localStorage.setItem(noti.Id.toString(), JSON.stringify(noti)); 
+    noticias.forEach(function(n){
+      localStorage.setItem(n.Id.toString(),JSON.stringify(n));
+      console.log(n);
+    });
+       
   }else{
     throw new Error('No se puede almacenar la información, porque no está habilitado el localStorage');
   }
@@ -30,13 +71,18 @@ setObjectValue = (key:string, objectValue:any)=>{
 
 
   getObjectValue = (key:string) => {
+    let noticias:any[] = []; 
     if (window.localStorage) {
-      const DATA= JSON.parse(localStorage.getItem('1'));
-      if (DATA) {
-        return DATA;
+      for(let i=1; i<=4; i++){
+        const DATA = JSON.parse(localStorage.getItem(i.toString()));
+        console.log(DATA);
+        noticias.push(DATA);
+      }
+     if (noticias) {
+        return noticias;
       }else{
         throw new Error('No se encontró el valor ${key} en el localStorage');
-      }
+      } 
     }else{
       throw new Error('No se puede obtener la información, porque no está habilitado el localStorage');
     }
